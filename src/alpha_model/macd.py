32397836117a3e6c63606ba87e5bfd6749b20729
milinda-abay <43531318+milinda-abay@users.noreply.github.com>
2024-07-16 23:56:23 +1000

    Merge dev to main (#1)

--- src/alpha_model/macd.py
+++ src/alpha_model/macd.py
@@ -1,15 +1,24 @@
 from settings import DATA_PATH
+import cudf 
 import pandas as pd
 import talib
 from matplotlib import pyplot as plt
 
 BTCUSDT_1M = DATA_PATH / "output" / "BTCUSDT_1m.parquet"
 
-df = pd.read_parquet(BTCUSDT_1M)
+df = cudf.read_parquet(BTCUSDT_1M)
 
+macd_fast,macd_slow,signal_period = 12,16,9
 
-df['macd'],df['macd_signal'], df['macd_hist'] = talib.MACD(df['close'])
+df['macd'],df['macd_signal'], df['macd_hist'] = talib.MACD(df['close'].to_pandas(),macd_fast,macd_slow,signal_period )
 
-df[['macd','macd_signal', 'macd_hist'] ]
-fig = plt.figure()
-fig.show()
+df = df.reset_index(drop=True)
+
+df.to_pandas()[['macd','macd_signal', 'macd_hist'] ].plot()
+plt.show()
+
+df.dtypes
+df['open_time'] = df['open_time'] /1000
+df['decimal'] = df.open_time.astype(int)
+
+(df['open_time'] == df['decimal']).all()
